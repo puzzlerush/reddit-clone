c627beddec272889798febefa009c8313841c61a
tacticaltofu <dingkevin@gmail.com>
2021-04-21 22:09:09 -0400

    Add seed-down sql script

--- backend/migrations/sqls/20210422010902-seed-down.sql
+++ backend/migrations/sqls/20210422010902-seed-down.sql
@@ -1,1 +1,8 @@
-/* Replace with your SQL commands */
+drop table moderators;
+drop table post_votes;
+drop table comments;
+drop table posts;
+drop type post_type;
+drop table subreddits;
+drop table users;
+drop function update_updated_at_column;
