b4e3205b97490cc6474dc615c4f36e2cb55afbf8
mlgmontage <kettleofrassel@gmail.com>
2022-03-09 22:11:18 +0600

    refactor: PublicRoute using hooks

--- frontend/src/components/PublicRoute.js
+++ frontend/src/components/PublicRoute.js
@@ -1,14 +1,11 @@
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import { tokenSelector } from '../selectors';
 
-const PublicRoute = ({ token, children, ...rest }) => (
-  <Route {...rest}>{token ? <Redirect to="/" /> : children}</Route>
-);
+const PublicRoute = ({ children, ...rest }) => {
+  const token = useSelector(tokenSelector);
+  return <Route {...rest}>{token ? <Redirect to="/" /> : children}</Route>;
+};
 
-const mapStateToProps = (state) => ({
-  token: tokenSelector(state),
-});
-
-export default connect(mapStateToProps)(PublicRoute);
+export default PublicRoute;
