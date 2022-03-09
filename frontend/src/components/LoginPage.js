4156c5efc4fb3cc42635ed618f2e1aecf49d9a2e
mlgmontage <kettleofrassel@gmail.com>
2022-03-09 21:59:55 +0600

    refactor: LoginPage using hooks

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import {
   Box,
@@ -13,22 +13,22 @@ import {
 import { createLoadingAndErrorSelector } from '../selectors';
 import { startLogin } from '../actions/auth';
 
-class LoginPage extends React.Component {
-  constructor(props) {
-    super(props);
+const { loadingSelector, errorSelector } = createLoadingAndErrorSelector(
+  ['LOGIN'],
+  false
+);
 
-    this.state = {
-      username: '',
-      password: '',
-    };
-  }
+const LoginPage = (props) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const isLoading = useSelector(loadingSelector);
+  const error = useSelector(errorSelector);
+  const dispatch = useDispatch();
 
-  handleSubmit = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const { username, password } = this.state;
-    const { startLogin, location, history } = this.props;
-    await startLogin(username, password);
-    const { error } = this.props;
+    const { location, history } = props;
+    await dispatch(startLogin(username, password));
     if (!error) {
       history.push(
         (location && location.state && location.state.prevPathname) || '/'
@@ -36,75 +36,56 @@ class LoginPage extends React.Component {
     }
   };
 
-  render() {
-    const { username, password } = this.state;
-    const { isLoading, error, location } = this.props;
-    const requireAuth =
-      location && location.state && location.state.requireAuth;
-    return (
-      <Box w={300} m="auto">
-        {requireAuth && (
-          <Alert status="warning" mb={2}>
-            <AlertIcon />
-            {requireAuth}
-          </Alert>
-        )}
-        {error && (
-          <Alert status="error" mb={2}>
-            <AlertIcon />
-            {error}
-          </Alert>
-        )}
-        <form onSubmit={this.handleSubmit}>
-          <Stack spacing={3}>
-            <FormControl>
-              <Input
-                value={username}
-                onChange={(e) => this.setState({ username: e.target.value })}
-                id="username-input"
-                variant="filled"
-                type="text"
-                placeholder="username"
-                size="md"
-                isRequired
-              />
-            </FormControl>
-            <FormControl>
-              <Input
-                value={password}
-                onChange={(e) => this.setState({ password: e.target.value })}
-                id="password-input"
-                variant="filled"
-                type="password"
-                placeholder="password"
-                size="md"
-                isRequired
-              />
-            </FormControl>
-            <Button type="submit" isLoading={isLoading || null}>
-              Login
-            </Button>
-          </Stack>
-        </form>
-      </Box>
-    );
-  }
-}
+  const { location } = props;
+  const requireAuth = location && location.state && location.state.requireAuth;
 
-const { loadingSelector, errorSelector } = createLoadingAndErrorSelector(
-  ['LOGIN'],
-  false
-);
+  return (
+    <Box w={300} m="auto">
+      {requireAuth && (
+        <Alert status="warning" mb={2}>
+          <AlertIcon />
+          {requireAuth}
+        </Alert>
+      )}
+      {error && (
+        <Alert status="error" mb={2}>
+          <AlertIcon />
+          {error}
+        </Alert>
+      )}
+      <form onSubmit={handleSubmit}>
+        <Stack spacing={3}>
+          <FormControl>
+            <Input
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              id="username-input"
+              variant="filled"
+              type="text"
+              placeholder="username"
+              size="md"
+              isRequired
+            />
+          </FormControl>
+          <FormControl>
+            <Input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              id="password-input"
+              variant="filled"
+              type="password"
+              placeholder="password"
+              size="md"
+              isRequired
+            />
+          </FormControl>
+          <Button type="submit" isLoading={isLoading || null}>
+            Login
+          </Button>
+        </Stack>
+      </form>
+    </Box>
+  );
+};
 
-const mapStateToProps = (state) => ({
-  isLoading: loadingSelector(state),
-  error: errorSelector(state),
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  startLogin: (username, password) => dispatch(startLogin(username, password)),
-});
-
-export default withRouter(
-  connect(mapStateToProps, mapDispatchToProps)(LoginPage)
-);
+export default withRouter(LoginPage);
