a50215d468c2c2a32c040c216ea72a7ff7699e0c
mlgmontage <kettleofrassel@gmail.com>
2022-03-10 21:57:48 +0600

    refactor: CreatePostPage using hooks

--- frontend/src/components/CreatePostPage.js
+++ frontend/src/components/CreatePostPage.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import {
   Box,
   Stack,
@@ -22,151 +22,126 @@ import {
 import { getSubreddits } from '../actions/subreddits';
 import { submitPost } from '../actions/post';
 
-class CreatePostPage extends React.Component {
-  constructor(props) {
-    super(props);
+const CreatePostPage = (props) => {
+  const [postType, setPostType] = useState('text');
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [url, setUrl] = useState('');
+  const [subreddit, setSubreddit] = useState('');
 
-    this.state = {
-      postType: 'text',
-      title: '',
-      body: '',
-      url: '',
-      subreddit: '',
-    };
-  }
+  const srIsLoading = useSelector(srLoadingSelector);
+  const srError = useSelector(srErrorSelector);
+  const submitIsLoading = useSelector(submitLoadingSelector);
+  const submitError = useSelector(submitErrorSelector);
+  const subreddits = useSelector(subredditsSelector);
 
-  componentDidMount() {
-    const { getSubreddits } = this.props;
-    getSubreddits();
-  }
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-  handleSubmit = async (e) => {
+  useEffect(() => {
+    dispatch(getSubreddits());
+  }, []);
+
+  const handleSubmit = async (e) => {
     try {
       e.preventDefault();
-      const { postType, title, body, url, subreddit } = this.state;
-      const { submitPost, history } = this.props;
-      const { id } = await submitPost({
-        type: postType,
-        title,
-        body: postType === 'text' ? body : url,
-        subreddit,
-      });
+      const { id } = await dispatch(
+        submitPost({
+          type: postType,
+          title,
+          body: postType === 'text' ? body : url,
+          subreddit,
+        })
+      );
       history.push(`/r/${subreddit}/comments/${id}`);
     } catch (err) {}
   };
 
-  render() {
-    const { postType, title, body, url, subreddit } = this.state;
-    const {
-      srIsLoading,
-      srError,
-      submitIsLoading,
-      submitError,
-      subreddits,
-    } = this.props;
-    return (
-      <Box w={['100%', '90%', '80%', '70%']} m="auto">
-        {submitError && (
-          <Alert status="error" mb={4}>
-            <AlertIcon />
-            {submitError}
-          </Alert>
-        )}
-        <form onSubmit={this.handleSubmit}>
-          <Stack spacing={3}>
-            <FormControl>
-              <RadioGroup
-                value={postType}
-                onChange={(postType) => this.setState({ postType })}
-              >
-                <Stack direction="row" spacing={3}>
-                  <Radio value="text">text post</Radio>
-                  <Radio value="link">link</Radio>
-                </Stack>
-              </RadioGroup>
-            </FormControl>
-            <FormControl>
-              <Input
-                value={title}
-                onChange={(e) => this.setState({ title: e.target.value })}
-                type="text"
-                variant="filled"
-                placeholder="title"
-                isRequired
-              />
-            </FormControl>
-            <FormControl>
-              {postType === 'text' ? (
-                <Textarea
-                  value={body}
-                  onChange={(e) => this.setState({ body: e.target.value })}
-                  variant="filled"
-                  placeholder="text (optional)"
-                  rows={10}
-                />
-              ) : (
-                <Input
-                  value={url}
-                  onChange={(e) => this.setState({ url: e.target.value })}
-                  type="url"
-                  variant="filled"
-                  placeholder="url"
-                  required
-                />
-              )}
-            </FormControl>
-            <FormControl isInvalid={srError}>
-              <Select
-                value={subreddit}
-                onChange={(e) => this.setState({ subreddit: e.target.value })}
-                variant="filled"
-                placeholder={srIsLoading ? 'loading...' : 'choose a subreddit'}
-                isRequired
-              >
-                {subreddits.map(({ name }) => (
-                  <option key={name} value={name}>
-                    {name}
-                  </option>
-                ))}
-              </Select>
-              <FormErrorMessage>Could not load subreddits</FormErrorMessage>
-            </FormControl>
-            <Button
-              type="submit"
-              isLoading={srIsLoading || submitIsLoading || null}
+  return (
+    <Box w={['100%', '90%', '80%', '70%']} m="auto">
+      {submitError && (
+        <Alert status="error" mb={4}>
+          <AlertIcon />
+          {submitError}
+        </Alert>
+      )}
+      <form onSubmit={handleSubmit}>
+        <Stack spacing={3}>
+          <FormControl>
+            <RadioGroup
+              value={postType}
+              onChange={(postType) => setPostType(postType)}
             >
-              Submit
-            </Button>
-          </Stack>
-        </form>
-      </Box>
-    );
-  }
-}
+              <Stack direction="row" spacing={3}>
+                <Radio value="text">text post</Radio>
+                <Radio value="link">link</Radio>
+              </Stack>
+            </RadioGroup>
+          </FormControl>
+          <FormControl>
+            <Input
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              type="text"
+              variant="filled"
+              placeholder="title"
+              isRequired
+            />
+          </FormControl>
+          <FormControl>
+            {postType === 'text' ? (
+              <Textarea
+                value={body}
+                onChange={(e) => setBody(e.target.value)}
+                variant="filled"
+                placeholder="text (optional)"
+                rows={10}
+              />
+            ) : (
+              <Input
+                value={url}
+                onChange={(e) => setUrl(e.target.value)}
+                type="url"
+                variant="filled"
+                placeholder="url"
+                required
+              />
+            )}
+          </FormControl>
+          <FormControl isInvalid={srError}>
+            <Select
+              value={subreddit}
+              onChange={(e) => setSubreddit(e.target.value)}
+              variant="filled"
+              placeholder={srIsLoading ? 'loading...' : 'choose a subreddit'}
+              isRequired
+            >
+              {subreddits.map(({ name }) => (
+                <option key={name} value={name}>
+                  {name}
+                </option>
+              ))}
+            </Select>
+            <FormErrorMessage>Could not load subreddits</FormErrorMessage>
+          </FormControl>
+          <Button
+            type="submit"
+            isLoading={srIsLoading || submitIsLoading || null}
+          >
+            Submit
+          </Button>
+        </Stack>
+      </form>
+    </Box>
+  );
+};
 
-const {
-  loadingSelector: srLoadingSelector,
-  errorSelector: srErrorSelector,
-} = createLoadingAndErrorSelector(['GET_SUBREDDITS']);
+const { loadingSelector: srLoadingSelector, errorSelector: srErrorSelector } =
+  createLoadingAndErrorSelector(['GET_SUBREDDITS']);
 
 const {
   loadingSelector: submitLoadingSelector,
   errorSelector: submitErrorSelector,
 } = createLoadingAndErrorSelector(['SUBMIT_POST'], false);
 
-const mapStateToProps = (state) => ({
-  srIsLoading: srLoadingSelector(state),
-  srError: srErrorSelector(state),
-  submitIsLoading: submitLoadingSelector(state),
-  submitError: submitErrorSelector(state),
-  subreddits: subredditsSelector(state),
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  getSubreddits: () => dispatch(getSubreddits()),
-  submitPost: (postDetails) => dispatch(submitPost(postDetails)),
-});
-
-export default withRouter(
-  connect(mapStateToProps, mapDispatchToProps)(CreatePostPage)
-);
+export default CreatePostPage;
