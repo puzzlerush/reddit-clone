04857f292bb699d282d76e45ebc9fa5d71ed72a2
mlgmontage <kettleofrassel@gmail.com>
2022-03-10 22:05:23 +0600

    refactor: CreateSubredditPage using hooks

--- frontend/src/components/CreateSubredditPage.js
+++ frontend/src/components/CreateSubredditPage.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import {
   Box,
   Stack,
@@ -15,94 +15,77 @@ import {
 import { createLoadingAndErrorSelector } from '../selectors';
 import { createSubreddit } from '../actions/subreddits';
 
-class CreateSubredditPage extends React.Component {
-  constructor(props) {
-    super(props);
+const CreateSubredditPage = (props) => {
+  const history = useHistory();
+  const dispatch = useDispatch();
 
-    this.state = {
-      name: '',
-      description: '',
-    };
-  }
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const isLoading = useSelector(loadingSelector);
+  const error = useSelector(errorSelector);
 
-  isNameValid = (name) => {
+  const isNameValid = (name) => {
     const nameRegex = new RegExp('^[a-z0-9]+$', 'i');
     return nameRegex.test(name);
   };
 
-  handleSubmit = async (e) => {
+  const handleSubmit = async (e) => {
     try {
       e.preventDefault();
-      const { name, description } = this.state;
-      const { createSubreddit, history } = this.props;
-      const { name: subredditName } = await createSubreddit(name, description);
+      const { name: subredditName } = await dispatch(
+        createSubreddit(name, description)
+      );
       history.push(`/r/${subredditName}`);
     } catch (e) {}
   };
 
-  render() {
-    const { name, description } = this.state;
-    const { isLoading, error } = this.props;
-    return (
-      <Box w={['100%', '90%', '80%', '70%']} m="auto">
-        {error && (
-          <Alert status="error" mb={4}>
-            <AlertIcon />
-            {error}
-          </Alert>
-        )}
-        <form onSubmit={this.handleSubmit}>
-          <Stack>
-            <FormControl isInvalid={name.length > 0 && !this.isNameValid(name)}>
-              <Input
-                value={name}
-                onChange={(e) => this.setState({ name: e.target.value })}
-                variant="filled"
-                placeholder="subreddit name"
-                isRequired
-              />
-              <FormErrorMessage>
-                Name can only contain alphanumeric characters
-              </FormErrorMessage>
-            </FormControl>
-            <FormControl>
-              <Textarea
-                value={description}
-                onChange={(e) => this.setState({ description: e.target.value })}
-                variant="filled"
-                rows={5}
-                placeholder="description (optional)"
-              />
-            </FormControl>
-            <Button
-              isLoading={isLoading}
-              type="submit"
-              isDisabled={!this.isNameValid(name)}
-            >
-              create
-            </Button>
-          </Stack>
-        </form>
-      </Box>
-    );
-  }
-}
+  return (
+    <Box w={['100%', '90%', '80%', '70%']} m="auto">
+      {error && (
+        <Alert status="error" mb={4}>
+          <AlertIcon />
+          {error}
+        </Alert>
+      )}
+      <form onSubmit={handleSubmit}>
+        <Stack>
+          <FormControl isInvalid={name.length > 0 && !isNameValid(name)}>
+            <Input
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              variant="filled"
+              placeholder="subreddit name"
+              isRequired
+            />
+            <FormErrorMessage>
+              Name can only contain alphanumeric characters
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl>
+            <Textarea
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              variant="filled"
+              rows={5}
+              placeholder="description (optional)"
+            />
+          </FormControl>
+          <Button
+            isLoading={isLoading}
+            type="submit"
+            isDisabled={!isNameValid(name)}
+          >
+            create
+          </Button>
+        </Stack>
+      </form>
+    </Box>
+  );
+};
 
 const { loadingSelector, errorSelector } = createLoadingAndErrorSelector(
   ['CREATE_SUBREDDIT'],
   false
 );
 
-const mapStateToProps = (state) => ({
-  isLoading: loadingSelector(state),
-  error: errorSelector(state),
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  createSubreddit: (name, description) =>
-    dispatch(createSubreddit(name, description)),
-});
-
-export default withRouter(
-  connect(mapStateToProps, mapDispatchToProps)(CreateSubredditPage)
-);
+export default CreateSubredditPage;
