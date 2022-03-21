76c7363289f72b6d85a173dc08137418a8d87fb0
mlgmontage <kettleofrassel@gmail.com>
2022-03-21 21:54:56 +0600

    refactor: WriteCommentBox formik refactor

--- frontend/src/components/WriteCommentBox.js
+++ frontend/src/components/WriteCommentBox.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Formik } from 'formik';
 import {
   Box,
   HStack,
@@ -17,54 +18,62 @@ const { loadingSelector, errorSelector } = createLoadingAndErrorSelector(
 );
 const WriteCommentBox = (props) => {
   const dispatch = useDispatch();
-  const [body, setBody] = useState('');
   const [hasError, setHasError] = useState(false);
   const isLoading = useSelector(loadingSelector);
   const error = useSelector(errorSelector);
   const user = useSelector(userSelector);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleSubmit = async (values, { resetForm }) => {
     const { postId, parentCommentId, onClose } = props;
     await dispatch(
       submitComment({
-        body,
+        body: values.comment,
         post_id: postId,
         parent_comment_id: parentCommentId,
       })
     );
-    setBody('');
     if (error) {
       setHasError(true);
     }
     if (!error && onClose) {
       onClose();
     }
+    resetForm();
   };
 
   const { type = 'comment', onClose } = props;
   const isReply = type === 'reply';
   return (
     <Box>
-      <form onSubmit={handleSubmit}>
-        <FormControl mb={3} isInvalid={error && hasError}>
-          <Textarea
-            value={body}
-            onChange={(e) => setBody(e.target.value)}
-            variant="filled"
-            isDisabled={!user}
-            placeholder="what are your thoughts?"
-            rows={5}
-          />
-          <FormErrorMessage>{error}</FormErrorMessage>
-        </FormControl>
-        <HStack>
-          <Button isDisabled={!body} isLoading={isLoading} type="submit">
-            {type}
-          </Button>
-          {isReply && onClose && <Button onClick={onClose}>cancel</Button>}
-        </HStack>
-      </form>
+      <Formik initialValues={{ comment: '' }} onSubmit={handleSubmit}>
+        {({ values, handleSubmit, handleChange, handleBlur, isSubmitting }) => (
+          <form onSubmit={handleSubmit}>
+            <FormControl mb={3} isInvalid={error && hasError}>
+              <Textarea
+                name="comment"
+                value={values.comment}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                variant="filled"
+                isDisabled={!user}
+                placeholder="what are your thoughts?"
+                rows={5}
+              />
+              <FormErrorMessage>{error}</FormErrorMessage>
+            </FormControl>
+            <HStack>
+              <Button
+                isDisabled={isSubmitting}
+                isLoading={isLoading}
+                type="submit"
+              >
+                {type}
+              </Button>
+              {isReply && onClose && <Button onClick={onClose}>cancel</Button>}
+            </HStack>
+          </form>
+        )}
+      </Formik>
     </Box>
   );
 };
